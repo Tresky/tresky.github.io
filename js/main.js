49c34987b0cc3c584922e30a20a6d22e60d0e2d2
Matt Webb <mattrw89@gmail.com>
2017-03-20 11:37:57 -0400

    Update publishable stripe key to prod

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ function setupRegisterForm() {
   }
 
   var handler = StripeCheckout.configure({
-    key: 'pk_test_GcdOiiCVGjbgaFj5UeVZHKkC',
+    key: 'pk_live_uMWLveljdrVLMTFgnPSzzECe',
     image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
     locale: 'auto',
     zipCode: true,
